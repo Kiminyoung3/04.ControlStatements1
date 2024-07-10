2c3c819e8b1f3f26b4e0fcaa2c1ee4b9dd745999
Kiminyoung3 <pie235@naver.com>
2024-07-10 16:41:39 +0900

    q

--- ControlStatements_2.py
+++ ControlStatements_2.py
@@ -1,2 +1,19 @@
-#else
+apple = "사과"
+banana = "바나나"
 
+if apple == "사과" and banana == "바나나" :
+    print("문자열이 모두 정확합니다.")
+elif apple != "사과" and banana == "바나나" :
+    print("사과가 없습니다.")
+elif apple == "사과" and banana != "바나나" :
+    print("바나나가 없습니다.")
+else :
+    print("과일을 입력하세요.")
+
+
+
+var = [1, 2, 3]
+if 3 in var:
+    print("숫자 3이 var 변수에 있습니다.")
+
+tar = {""}
